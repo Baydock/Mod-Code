ed8f0fe52b1d6f58f649eb8d8b90facbc9f13022
baydock <41644363+baydock@users.noreply.github.com>
2021-10-31 15:22:49 -0500

    added Swap Land and Water Mod

---
+++ Swap Land and Water/Test Mod/Test Mod.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts.Models.Map;
+using Assets.Scripts.Models.Towers;
+using HarmonyLib;
+using System.Collections.Generic;
+
+namespace SwapLandAndWater {
+    public class Mod : MelonLoader.MelonMod {
+        public override void OnApplicationStart() {
+        }
+    }
+
+    [HarmonyPatch(typeof(GameModelLoader), nameof(GameModelLoader.Load))]
+    public class GameModelLoad_Patch {
+        [HarmonyPostfix]
+        public static void Postfix(ref Assets.Scripts.Models.GameModel __result) {
+            for (int i = 0; i < __result.towers.Length; i++) {
+                TowerModel tower = __result.towers[i];
+                if (tower.areaTypes.Contains(AreaType.land) && !tower.areaTypes.Contains(AreaType.water)) {
+                    List<AreaType> areaTypes = new List<AreaType>(tower.areaTypes);
+                    areaTypes.Remove(AreaType.land);
+                    areaTypes.Add(AreaType.water);
+                    tower.areaTypes = areaTypes.ToArray();
+                } else if (tower.areaTypes.Contains(AreaType.water) && !tower.areaTypes.Contains(AreaType.land)) {
+                    List<AreaType> areaTypes = new List<AreaType>(tower.areaTypes);
+                    areaTypes.Remove(AreaType.water);
+                    areaTypes.Add(AreaType.land);
+                    tower.areaTypes = areaTypes.ToArray();
+                }
+            }
+        }
+    }
+}
