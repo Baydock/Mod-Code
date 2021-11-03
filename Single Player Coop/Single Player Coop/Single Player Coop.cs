1962172c1380e30f9a43d149ddf200a7b5491c2c
baydock <41644363+baydock@users.noreply.github.com>
2021-11-03 16:55:59 -0500

    added existing mods that still work

---
+++ Single Player Coop/Single Player Coop/Single Player Coop.cs
@@ -0,0 +1,52 @@
+﻿using Assets.Scripts.Unity.UI_New.Coop;
+using Harmony;
+using MelonLoader;
+using System.Linq;
+using UnityEngine.UI;
+
+namespace SinglePlayerCoop {
+    public class SinglePlayerCoopMod : MelonMod { }
+
+    [HarmonyPatch(typeof(CoopLobbyScreen), nameof(CoopLobbyScreen.SetReadyAndPublicButton))]
+    public class CoopReadyAndCoopButton_Patch {
+        [HarmonyPatch]
+        public static void Postfix(CoopLobbyScreen __instance) {
+            var f = X.X<bool, bool, bool, bool, bool, bool>.Y.y;
+            var buttons = __instance.readyButton.GetComponentsInChildren<Button>()
+                .Concat(__instance.publicButton.GetComponentsInChildren<Button>());
+            foreach (var button in buttons) {
+                button.enabled = true;
+                button.interactable = true;
+                button.gameObject.SetActive(true);
+                button.transform.parent.gameObject.SetActive(true);
+            }
+            __instance.lockedInfoButton.SetActive(false);
+            f = X.X<bool, bool, bool, bool, bool, bool>.Y.y;
+        }
+    }
+
+    [HarmonyPatch(typeof(CoopLobbyScreen), nameof(CoopLobbyScreen.OnMakePublicClicked))]
+    public class CoopPublicClicked_Patch {
+        [HarmonyPatch]
+        public static void Postfix(CoopLobbyScreen __instance) {
+            var f = X.X<bool, bool, bool, bool, bool, bool>.Y.y;
+            var buttons = __instance.readyButton.GetComponentsInChildren<Button>();
+            foreach (var button in buttons) {
+                button.enabled = true;
+                button.interactable = true;
+                button.gameObject.SetActive(true);
+            }
+            __instance.lockedInfoButton.SetActive(false);
+            __instance.publicButton.SetActive(false);
+            f = X.X<bool, bool, bool, bool, bool, bool>.Y.y;
+        }
+    }
+}
+
+namespace X {
+    public class X<A, B, C, D, E, F> {
+        public class Y : X<Y, Y, Y, Y, Y, Y> {
+            public static Y.Y.Y.Y.Y.Y.Y y;
+        }
+    }
+}
---
+++ Sussymongus/Sussymongus/SussymongusMod.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Main.Scenes;
+using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+
+namespace Sussymongus {
+    public class SussymongusMod : MelonMod {
+        public override void OnApplicationStart() {
+            MelonLogger.Msg("Hello World!");
+        }
+    }
+
+    [HarmonyPatch(typeof(TitleScreen), nameof(TitleScreen.OnPlayButtonClicked))]
+    public class TitleScreenPatch {
+        [HarmonyPrefix]
+        public static bool Prefix() {
+            Application.OpenURL("https://store.steampowered.com/app/945360/Among_Us/");
+            Application.Quit();
+            return false;
+        }
+    }
+}
