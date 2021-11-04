85c42fdad25bc77eaaba041f34c4ac63ce07e2cc
baydock <41644363+baydock@users.noreply.github.com>
2021-11-04 17:38:30 -0500

    a

--- Helpful Additions/Helpful Additions/Properties/Textures.Designer.cs
+++ Helpful Additions/Helpful Additions/Properties/Textures.Designer.cs
@@ -39,7 +39,7 @@ namespace HelpfulAdditions.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AdditionalSandboxButtons.Properties.Textures", typeof(Textures).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HelpfulAdditions.Properties.Textures", typeof(Textures).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
