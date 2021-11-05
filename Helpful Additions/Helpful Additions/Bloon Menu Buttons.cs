a914c4977b23c38f6f1ea512935b8dfe82216198
baydock <41644363+baydock@users.noreply.github.com>
2021-11-04 20:31:48 -0500

    added single player coop to helpful additions

---
+++ Helpful Additions/Helpful Additions/Bloon Menu Buttons.cs
@@ -0,0 +1,79 @@
+﻿using Assets.Scripts.Unity.UI_New;
+using Assets.Scripts.Unity.UI_New.InGame;
+using Assets.Scripts.Unity.UI_New.InGame.BloonMenu;
+using HarmonyLib;
+using HelpfulAdditions.Properties;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace HelpfulAdditions {
+    public static partial class Globals {
+        internal static Dictionary<int, GameObject> destroyProjectilesButtons = new Dictionary<int, GameObject>();
+    }
+
+    [HarmonyPatch(typeof(BloonMenu), nameof(BloonMenu.Initialise))]
+    public class AddButtons_Patch {
+        [HarmonyPostfix]
+        public static void Postfix(ref BloonMenu __instance) {
+            GameObject destroyProjectilesButton = Object.Instantiate(__instance.destroyTowersButton, __instance.transform);
+            destroyProjectilesButton.name = "DestroyProjectilesButton";
+
+            ButtonExtended button = destroyProjectilesButton.GetComponent<ButtonExtended>();
+            button.OnPointerUpEvent = new System.Action<PointerEventData>((PointerEventData p) => InGame.Bridge.DestroyAllProjectiles());
+
+            Image image = destroyProjectilesButton.GetComponent<Image>();
+            Texture2D tex = new Texture2D(0, 0);
+            using (MemoryStream ms = new MemoryStream()) {
+                Textures.deleteProjectiles.Save(ms, ImageFormat.Png);
+                ImageConversion.LoadImage(tex, ms.ToArray());
+                ms.Close();
+            }
+            image.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2());
+
+            Globals.destroyProjectilesButtons.Add(__instance.GetInstanceID(), destroyProjectilesButton);
+
+            // Allows for animator to only be updated manually
+            // This is necessary because, for whatever reason, patching Animator.Update does nothing
+            __instance.animator.enabled = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(BloonMenu), nameof(BloonMenu.Destroy))]
+    public class RemoveButtons_Patch {
+        [HarmonyPostfix]
+        public static void Postfix(ref BloonMenu __instance) {
+            int instanceId = __instance.GetInstanceID();
+            GameObject projectileButton = Globals.destroyProjectilesButtons[instanceId];
+            Object.Destroy(projectileButton);
+            Globals.destroyProjectilesButtons.Remove(instanceId);
+        }
+    }
+
+    [HarmonyPatch(typeof(BloonMenu), nameof(BloonMenu.Update))]
+    public class UpdateButtons_Patch {
+        private static float oldTime = 0;
+        [HarmonyPostfix]
+        public static void Postfix(ref BloonMenu __instance) {
+            // Manually updating animator
+            float newTime = Time.time;
+            __instance.animator.Update(newTime - oldTime);
+            oldTime = newTime;
+
+            float spacing = __instance.btnDestroyBloons.transform.position.y - __instance.btnDestroyMonkeys.transform.position.y;
+            Vector3 destroyProjectilesPos = __instance.btnDestroyMonkeys.transform.position;
+
+            __instance.btnResetDamage.transform.position += new Vector3(0, spacing);
+            __instance.btnResetAbilityCooldowns.transform.position += new Vector3(0, spacing);
+            __instance.btnDestroyBloons.transform.position += new Vector3(0, spacing);
+            __instance.btnDestroyMonkeys.transform.position += new Vector3(0, spacing);
+
+            int instanceId = __instance.GetInstanceID();
+            if (Globals.destroyProjectilesButtons.ContainsKey(instanceId))
+                Globals.destroyProjectilesButtons[instanceId].transform.position = destroyProjectilesPos;
+        }
+    }
+}
---
+++ Helpful Additions/Helpful Additions/Single Player Coop.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Unity.UI_New.Coop;
+using HarmonyLib;
+using UnityEngine.UI;
+
+namespace HelpfulAdditions {
+
+    [HarmonyPatch(typeof(CoopLobbyScreen), nameof(CoopLobbyScreen.SetReadyAndPublicButton))]
+    public class CoopReadyAndCoopButton_Patch {
+        [HarmonyPatch]
+        public static void Postfix(CoopLobbyScreen __instance) {
+            Button readyButton = __instance.readyButton.GetComponentInChildren<Button>();
+            readyButton.enabled = true;
+            readyButton.interactable = true;
+        }
+    }
+}
