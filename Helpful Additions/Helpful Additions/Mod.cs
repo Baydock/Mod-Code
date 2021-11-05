c4c1afbcf907ce73d3d06d551169b9b76535e499
baydock <41644363+baydock@users.noreply.github.com>
2021-11-05 10:48:27 -0500

    update version

--- Helpful Additions/Helpful Additions/Mod.cs
+++ Helpful Additions/Helpful Additions/Mod.cs
@@ -1,6 +1,6 @@
 ﻿using MelonLoader;
 
-[assembly: MelonInfo(typeof(HelpfulAdditions.Mod), "Helpful Additions", "1.1.0", "Baydock")]
+[assembly: MelonInfo(typeof(HelpfulAdditions.Mod), "Helpful Additions", "1.1.1", "Baydock")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 
 namespace HelpfulAdditions {
