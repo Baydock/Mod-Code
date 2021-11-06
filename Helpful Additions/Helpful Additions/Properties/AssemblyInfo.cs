fa17c98e06f76988fe64a7c5497ccae2d55e1ae9
baydock <41644363+baydock@users.noreply.github.com>
2021-11-05 21:06:26 -0500

    updated assembly info

--- Helpful Additions/Helpful Additions/Properties/AssemblyInfo.cs
+++ Helpful Additions/Helpful Additions/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Additional Sandbox Buttons")]
+[assembly: AssemblyTitle("Helpful Additions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Additional Sandbox Buttons")]
+[assembly: AssemblyCompany("Baydock")]
+[assembly: AssemblyProduct("Helpful Additions")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
